a8c91624cc2fc6aa6e94e32405c0b03651501efa
nicholasrwx <digitalexistence18@gmail.com>
2020-11-07 07:33:38 +0000

    Javascripting Files

---
+++ functions-arguments.js
@@ -0,0 +1,6 @@
+function math (firstArg, secondArg, thirdArg) {         //argument names are only used to reference them
+  let result = secondArg * thirdArg + firstArg
+  return result
+}
+
+console.log(math(53, 61, 67))
